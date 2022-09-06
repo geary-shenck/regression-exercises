796513dce2e9539a4717a816f864f6b23d083690
Geary Shenck <geary.shenck@gmail.com>
2022-09-06 12:19:55 -0500

    used supplied for reference, changed a few things for personal use

---
+++ wrangle.py
@@ -0,0 +1,153 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+
+# Generic splitting function for continuous target.
+
+def split_continuous(df):
+    """
+    Takes in a df
+    Returns train, validate, and test DataFrames
+    """
+    # Create train_validate and test datasets
+    train_validate, test = train_test_split(df, test_size=0.2, random_state=123)
+    # Create train and validate datsets
+    train, validate = train_test_split(train_validate, test_size=0.25, random_state=123)
+
+    # Take a look at your split datasets
+
+    print(f"train -> {train.shape}")
+    print(f"validate -> {validate.shape}")
+    print(f"test -> {test.shape}")
+    return train, validate, test
+
+
+def train_validate_test(df, target):
+    """
+    takes in a dataframe, splits it into 60, 20, 20, 
+    and seperates out the x variables and y (target) as new df/series
+    """
+    # split df into test (20%) and train_validate (80%)
+    train_validate, test = train_test_split(df, test_size=0.2, random_state=123)
+
+    # split train_validate off into train (70% of 80% = 56%) and validate (30% of 80% = 24%)
+    train, validate = train_test_split(train_validate, test_size=0.25, random_state=123)
+
+    # split train into X (dataframe, drop target) & y (series, keep target only)
+    X_train = train.drop(columns=[target])
+    y_train = train[target]
+
+    # split validate into X (dataframe, drop target) & y (series, keep target only)
+    X_validate = validate.drop(columns=[target])
+    y_validate = validate[target]
+
+    # split test into X (dataframe, drop target) & y (series, keep target only)
+    X_test = test.drop(columns=[target])
+    y_test = test[target]
+
+    print(f"train -> {train.shape}")
+    print(f"validate -> {validate.shape}")
+    print(f"test -> {test.shape}")
+
+    return X_train, y_train, X_validate, y_validate, X_test, y_test
+
+
+
+def get_numeric_X_cols(X_train, object_cols):
+    """
+    RUN THIS AFTER OBJECT COLUMNS
+    takes in a dataframe and list of object column names
+    and returns a list of all other columns names, the non-objects.
+    """
+    numeric_cols = [col for col in X_train.columns.values if col not in object_cols]
+
+    return numeric_cols
+
+
+def min_max_scale(X_train, X_validate, X_test, numeric_cols):
+    """
+    takes in the train data sets (3) and numeric column list,
+    and fits a min-max scaler to the first dataframe and transforms all 3
+    returns 3 dataframes with the same scaled values.
+    """
+    # create the scaler object and fit it to X_train (i.e. identify min and max)
+    # if copy = false, inplace row normalization happens and avoids a copy (if the input is already a numpy array).
+
+    scaler = MinMaxScaler(copy=True).fit(X_train[numeric_cols])
+
+    # scale X_train, X_validate, X_test using the mins and maxes stored in the scaler derived from X_train.
+    #
+    X_train_scaled_array = scaler.transform(X_train[numeric_cols])
+    X_validate_scaled_array = scaler.transform(X_validate[numeric_cols])
+    X_test_scaled_array = scaler.transform(X_test[numeric_cols])
+
+    # convert arrays to dataframes
+    X_train_scaled = pd.DataFrame(X_train_scaled_array, columns=numeric_cols).set_index(
+        [X_train.index.values]
+    )
+
+    X_validate_scaled = pd.DataFrame(
+        X_validate_scaled_array, columns=numeric_cols
+    ).set_index([X_validate.index.values])
+
+    X_test_scaled = pd.DataFrame(X_test_scaled_array, columns=numeric_cols).set_index(
+        [X_test.index.values]
+    )
+
+    return X_train_scaled, X_validate_scaled, X_test_scaled
+
+
+def get_object_cols(df):
+    """
+    This function takes in a dataframe and identifies the columns that are object types
+    and returns a list of those column names.
+    """
+    # create a mask of columns whether they are object type or not
+    mask = np.array(df.dtypes == "object")
+
+    # get a list of the column names that are objects (from the mask)
+    object_cols = df.iloc[:, mask].columns.tolist()
+
+    return object_cols
+
+
+
+
+def wrangle_example(path):
+    df = pd.read_csv(path, sep=";")
+
+    # drop any nulls
+    df = df[~df.isnull()]
+
+    # get object column names
+    object_cols = get_object_cols(df)
+
+    # create dummy vars
+    df = create_dummies(df, object_cols)
+
+    # split data
+    X_train, y_train, X_validate, y_validate, X_test, y_test = train_validate_test(
+        df, "G3"
+    )
+
+    # get numeric column names
+    numeric_cols = get_numeric_X_cols(X_train, object_cols)
+
+    # scale data
+    X_train_scaled, X_validate_scaled, X_test_scaled = min_max_scale(
+        X_train, X_validate, X_test, numeric_cols
+    )
+
+    return (
+        df,
+        X_train,
+        X_train_scaled,
+        y_train,
+        X_validate_scaled,
+        y_validate,
+        X_test_scaled,
+        y_test,
+    )
